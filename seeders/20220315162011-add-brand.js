1ef4df6b0ecabc596fa182986cd9b7cf18f12d9b
Jubel13 <jubelsinaga13@gmail.com>
2022-03-17 17:24:28 +0700

    Seed brand and type done

---
+++ seeders/20220315162011-add-brand.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const data = require("../data/brands.json");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    data.forEach((el) => {
+      el.createdAt = new Date();
+      el.updatedAt = new Date();
+    });
+
+    await queryInterface.bulkInsert("Brands", data, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Brands", null, {});
+  },
+};
