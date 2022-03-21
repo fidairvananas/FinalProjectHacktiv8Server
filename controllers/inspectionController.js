00c8d1123ecbc4c03e644d3920d7ceefacd9008f
Jubel13 <jubelsinaga13@gmail.com>
2022-03-21 16:39:51 +0700

    Refactor inspection code and test

--- controllers/inspectionController.js
+++ controllers/inspectionController.js
@@ -52,9 +52,15 @@ const getInspection = async (req, res, next) => {
   }
 };
 
-const changeMainInspection = async (req, res, next) => {
+const changeInspection = async (req, res, next) => {
   try {
-    const { mainInspection } = req.body;
+    const {
+      mainInspection,
+      exteriorInspection,
+      interiorInspection,
+      roadTest,
+      kolongTest,
+    } = req.body;
     const inspectionId = req.params.id;
     const { name } = req.loginAdmin;
 
@@ -68,125 +74,25 @@ const changeMainInspection = async (req, res, next) => {
     }
 
     await Inspection.update(
-      { mainInspection: mainInspection, inspectedBy: name },
+      {
+        mainInspection,
+        exteriorInspection,
+        interiorInspection,
+        roadTest,
+        kolongTest,
+        inspectedBy: name,
+      },
       { where: { id: inspectionId } }
     );
 
-    res.status(200).json({ message: "Main inspection updated" });
-  } catch (err) {
-    next(err);
-  }
-};
-
-const changeExterior = async (req, res, next) => {
-  try {
-    const { exteriorInspection } = req.body;
-    const inspectionId = req.params.id;
-    const inspection = await Inspection.findByPk(inspectionId);
-    const { name } = req.loginAdmin;
-
-    if (!inspection) {
-      throw {
-        code: 404,
-        name: "NOT_FOUND",
-        message: "Inspection not found",
-      };
-    }
-
-    await Inspection.update(
-      { exteriorInspection, inspectedBy: name },
-      { where: { id: inspectionId } }
-    );
-
-    res.status(200).json({ message: "Exterior inspection updated" });
-  } catch (err) {
-    next(err);
-  }
-};
-
-const changeInterior = async (req, res, next) => {
-  try {
-    const { interiorInspection } = req.body;
-    const inspectionId = req.params.id;
-    const inspection = await Inspection.findByPk(inspectionId);
-    const { name } = req.loginAdmin;
-
-    if (!inspection) {
-      throw {
-        code: 404,
-        name: "NOT_FOUND",
-        message: "Inspection not found",
-      };
-    }
-    await Inspection.update(
-      { interiorInspection, inspectedBy: name },
-      { where: { id: inspectionId } }
-    );
-
-    res.status(200).json({ message: "Interior inspection updated" });
-  } catch (err) {
-    next(err);
-  }
-};
-
-const changeRoadTest = async (req, res, next) => {
-  try {
-    const { roadTest } = req.body;
-    const inspectionId = req.params.id;
-    const inspection = await Inspection.findByPk(inspectionId);
-    const { name } = req.loginAdmin;
-
-    if (!inspection) {
-      throw {
-        code: 404,
-        name: "NOT_FOUND",
-        message: "Inspection not found",
-      };
-    }
-
-    await Inspection.update(
-      { roadTest, inspectedBy: name },
-      { where: { id: inspectionId } }
-    );
-
-    res.status(200).json({ message: "Road test inspection updated" });
-  } catch (err) {
-    next(err);
-  }
-};
-
-const changeKolong = async (req, res, next) => {
-  try {
-    const { kolongTest } = req.body;
-    const inspectionId = req.params.id;
-    const inspection = await Inspection.findByPk(inspectionId);
-    const { name } = req.loginAdmin;
-
-    if (!inspection) {
-      throw {
-        code: 404,
-        name: "NOT_FOUND",
-        message: "Inspection not found",
-      };
-    }
-
-    await Inspection.update(
-      { kolongTest, inspectedBy: name },
-      { where: { id: inspectionId } }
-    );
-
-    res.status(200).json({ message: "Kolong inspection updated" });
+    res.status(200).json({ message: "Inspection updated" });
   } catch (err) {
     next(err);
   }
 };
 
 module.exports = {
-  changeMainInspection,
-  changeExterior,
-  changeInterior,
-  changeKolong,
-  changeRoadTest,
+  changeInspection,
   getInspections,
   getInspection,
 };
